315fa11adad3335c055d837030e6bc51898533ab
sam38124 <sam38124@gmail.com>
2021-12-17 10:27:34 +0800

    1217_1

--- Glitterlib/src/main/java/com/jianzhi/glitter/plugins/SoundManager.kt
+++ Glitterlib/src/main/java/com/jianzhi/glitter/plugins/SoundManager.kt
@@ -1,38 +1,47 @@
 package com.jianzhi.glitter.plugins
 
 import android.media.MediaPlayer
+import android.net.Uri
 import com.jianzhi.glitter.GlitterActivity.Companion.instance
 import com.jianzhi.glitter.JavaScriptInterFace
+import java.io.File
 
 object SoundManager {
+    var mediiaplay: MediaPlayer? = null
     fun initial(){
-        var canPlayMedia=false
-        arrayOf(
-            /**
-            * 聲音播放 [rout:String]
-            * */
-            JavaScriptInterFace("SoundManager_Play"){
-                    request->
-                if (!canPlayMedia) {
-                    request.responseValue["result"]=false
-                    request.finish()
-                    return@JavaScriptInterFace
-                }
-                val rout=request.receiveValue["rout"].toString()
-                canPlayMedia = false
-                val assetRout = rout
-                val afd = instance().assets.openFd(assetRout);
-                val mediiaplay = MediaPlayer()
-                mediiaplay.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
-                mediiaplay.prepare()
-                mediiaplay.setOnCompletionListener {
-                    mediiaplay.release()
-                    canPlayMedia = true
-                }
-                mediiaplay.start()
-                request.responseValue["result"]=true
-                request.finish()
+        /**
+        * 聲音播放FromAssets
+        * */
+        JavaScriptInterFace("SoundManager_PlayAssets") { request ->
+            val rout = request.receiveValue["rout"].toString()
+            val afd = instance().assets.openFd(rout);
+            if (mediiaplay == null) {
+                mediiaplay = MediaPlayer()
+            } else {
+                mediiaplay!!.reset()
             }
-        ).map{}
+            mediiaplay!!.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+            mediiaplay!!.prepare()
+            mediiaplay!!.start()
+            request.responseValue["result"] = true
+            request.finish()
+        }
+        /**
+         * 聲音播放FromFile
+         * */
+        JavaScriptInterFace("SoundManager_PlayFile") { request ->
+            val rout = request.receiveValue["rout"].toString()
+            if (mediiaplay == null) {
+                mediiaplay = MediaPlayer()
+            } else {
+                mediiaplay!!.reset()
+            }
+
+            mediiaplay!!.setDataSource(instance().applicationContext ,  Uri.fromFile(File(instance().applicationContext.filesDir.absolutePath+"/${rout}")))
+            mediiaplay!!.prepare()
+            mediiaplay!!.start()
+            request.responseValue["result"] = true
+            request.finish()
+        }
     }
 }
